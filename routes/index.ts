94d474ef6022cbb529f79a28c7c6683c73084c0e
pBread <4473570+pBread@users.noreply.github.com>
2024-05-10 15:13:02 -0600

    working

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import crypto from "crypto";
 
 dotenv.config();
 
@@ -13,10 +14,16 @@ app.use(express.json());
 
 app.use("/post-event", async (req, res) => {
   console.log("=== post-event ===");
-  console.log("url\n", req.url);
-  console.log("body\n", req.body);
-  console.log("headers\n", req.headers);
-  console.log("===");
+  const twilioSignature = req.headers["x-twilio-signature"];
+  const url = "https://pbread.ngrok.io/post-event?hello=world";
+
+  const computedSignature = getExpectedSignature(AUTH_TOKEN, url, req.body);
+
+  console.log("twilioSignature", twilioSignature);
+  console.log("computedSignature", computedSignature);
+  console.log("do signatures match: ", computedSignature === twilioSignature);
+  console.log("url: ", url);
+  console.log("body: ", req.body);
 
   res.status(200).send({ hello: "world" }).end();
 });
@@ -24,3 +31,34 @@ app.use("/post-event", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server running on https://localhost:${PORT}`);
 });
+
+function getExpectedSignature(
+  authToken: string,
+  url: string,
+  params: Record<string, any>
+): string {
+  const data = Object.keys(params)
+    .sort()
+    .reduce((acc, key) => acc + toFormUrlEncodedParam(key, params[key]), url);
+
+  console.log("url given to signature", url);
+
+  return crypto
+    .createHmac("sha1", authToken)
+    .update(Buffer.from(data, "utf-8"))
+    .digest("base64");
+}
+
+function toFormUrlEncodedParam(
+  paramName: string,
+  paramValue: string | Array<string>
+): string {
+  if (paramValue instanceof Array) {
+    return Array.from(new Set(paramValue))
+      .sort()
+      .map((val) => toFormUrlEncodedParam(paramName, val))
+      .reduce((acc, val) => acc + val, "");
+  }
+
+  return paramName + paramValue;
+}
--- scripts/index.ts
+++ scripts/index.ts
@@ -6,13 +6,16 @@ dotenv.config();
 const { ACCOUNT_SID, AUTH_TOKEN, CONVO_SVC_SID, PHONE_NUMBERS } = process.env;
 const client = twilio(ACCOUNT_SID, AUTH_TOKEN);
 
+const convoSid = "CH33d1d6d422f54ae788cd71c15b5f2a6d";
+
 console.log("==script started==");
 (async () => {
   let data: { [key: string]: any } = {};
 
   data.convo = await client.conversations.v1
     .services(CONVO_SVC_SID)
-    .conversations.create({ friendlyName: new Date().toLocaleString() });
+    .conversations(convoSid)
+    .messages.create({ body: "Hello", xTwilioWebhookEnabled: "true" });
 
   console.log(data);
 })().then(() => console.log("==script stopped=="));
